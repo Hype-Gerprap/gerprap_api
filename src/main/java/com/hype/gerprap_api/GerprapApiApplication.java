557f6a133076ad80a00e4cf7179164c909816a16
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-10-26 17:39:18 -0300

    update

--- src/main/java/com/hype/gerprap_api/GerprapApiApplication.java
+++ src/main/java/com/hype/gerprap_api/GerprapApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerprapApiApplication {
 
+	//update
 	public static void main(String[] args) {
 		SpringApplication.run(GerprapApiApplication.class, args);
 	}
