b8786dcf078fd85b9f0ace7d1656afbebb50c4cb
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-10-26 14:34:50 -0300

    first commit

---
+++ src/main/java/com/hype/gerprap_api/GerprapApiApplication.java
@@ -0,0 +1,13 @@
+package com.hype.gerprap_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerprapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerprapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hype/gerprap_api/GerprapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hype.gerprap_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerprapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
