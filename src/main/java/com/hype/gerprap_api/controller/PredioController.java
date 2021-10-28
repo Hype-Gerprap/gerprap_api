54a84859aecd4c892c60bb32d914dfa92b040faa
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-10-28 17:31:52 -0300

    create. PredioController

--- src/main/java/com/hype/gerprap_api/controller/PredioController.java
+++ src/main/java/com/hype/gerprap_api/controller/PredioController.java
@@ -1,5 +1,70 @@
 package com.hype.gerprap_api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.hype.gerprap_api.entity.Predio;
+import com.hype.gerprap_api.repository.PredioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin
 public class PredioController {
-    
+ 
+    //instanciando o repositório do usuário para usar o banco (JPA-MySql):
+    @Autowired
+    private PredioRepository predioRepository;
+
+    // find all predios
+    @GetMapping("/all-predios")
+    public List<Predio> listPredios() {
+        return predioRepository.findAll();
+    }
+
+    // find predio by id
+    @GetMapping("/predio-id={id}")
+    public Optional<Predio> selectById(@PathVariable(value = "id") Long id) {
+        return predioRepository.findById(id);
+    }
+
+    // find predio (podem haver muitos "predios" com o mesmo "nome") by nome
+    @GetMapping("/predio-nome={nome}")
+    public List<Predio> selectByName(@PathVariable(value = "nome") String nome) {
+        return predioRepository.findAllByNome(nome);
+    }
+
+    // find predio by nome ("nome" deve ser uma "unique key")
+    @GetMapping("/predio-nome={nome}")
+    public Optional<Predio> selectByNome(@PathVariable(value = "nome") String nome) {
+        return predioRepository.findByNome(nome);
+    }
+
+    // insert new predio
+    @PostMapping("/insert-predio")
+    public Predio savePredio(@RequestBody Predio predio){
+				Optional<Predio> findbyNome = predioRepository.findByNome(predio.getNome());
+
+				if(findbyNome.isEmpty() == true){
+					return predioRepository.save(predio);
+				}else{
+					Predio predioNull = new Predio();
+					return predioNull;
+				}
+    }
+
+    // delete predio (VOID)
+    @DeleteMapping("/delete-predio={id}")
+    public void deletePredio(@PathVariable Long id){
+        predioRepository.deleteById(id);
+    }
+
+    // update predio
+    @PutMapping("/update-predio={id}")
+    public Predio updatePredio(@RequestBody Predio predio){
+				return predioRepository.save(predio);
+		}
+
 }
--- src/main/java/com/hype/gerprap_api/repository/PredioRepository.java
+++ src/main/java/com/hype/gerprap_api/repository/PredioRepository.java
@@ -1,5 +1,6 @@
 package com.hype.gerprap_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.hype.gerprap_api.entity.Predio;
@@ -8,6 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PredioRepository extends JpaRepository<Predio, Long> {
     
+    List<Predio> findAllByNome(String nome);
+
     Optional<Predio> findByNome(String nome);
+
 
 }
