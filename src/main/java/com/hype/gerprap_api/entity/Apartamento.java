f91f74f62ec3537b5dbb92e055d71cda79701f29
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-10-28 21:53:38 -0300

    update

--- src/main/java/com/hype/gerprap_api/entity/Apartamento.java
+++ src/main/java/com/hype/gerprap_api/entity/Apartamento.java
@@ -2,11 +2,9 @@ package com.hype.gerprap_api.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -15,7 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name ="apartamento")
+@Entity()
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
@@ -55,21 +53,6 @@ public class Apartamento {
     @Column(name = "area_apartamento", nullable = true, length = 1)
     private Double areaApartamento; 
     
-    @Getter
-    @Setter
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Predio predio;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Apartamento )) return false;
-        return id != null && id.equals(((Apartamento) o).getId());
-    }
- 
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
+    
 
 }
--- src/main/java/com/hype/gerprap_api/entity/Predio.java
+++ src/main/java/com/hype/gerprap_api/entity/Predio.java
@@ -1,27 +1,22 @@
 package com.hype.gerprap_api.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-//import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "predio")
+@Entity()
 @AllArgsConstructor
 @NoArgsConstructor
-//@EqualsAndHashCode
+@EqualsAndHashCode
 @Table(name = "predio")
 public class Predio {
 
@@ -59,24 +54,5 @@ public class Predio {
     @Column(name = "estado", nullable = false, length = 25)
     private String estado;
 
-    @Getter
-    @Setter
-    //@Column(name = "id_predio", nullable = false)
-    @OneToMany(
-        mappedBy = "predio",
-        cascade = CascadeType.ALL,
-        orphanRemoval = true
-    )
-    private List<Apartamento> apartamentos = new ArrayList<>();
- 
-    public void addApartamento(Apartamento apartamento) {
-        apartamentos.add(apartamento);
-        apartamento.setPredio(this);
-    }
- 
-    public void removeApartamento(Apartamento apartamento) {
-        apartamentos.remove(apartamento);
-        apartamento.setPredio(null);
-    }
 
 }
