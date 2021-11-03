683309a4862613026458d163b46a64a38ab29056
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-11-03 14:28:06 -0300

    update.

--- src/main/java/com/hype/gerprap_api/GerprapApiApplication.java
+++ src/main/java/com/hype/gerprap_api/GerprapApiApplication.java
@@ -3,6 +3,8 @@ package com.hype.gerprap_api;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.OpenAPI;
@@ -10,11 +12,26 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
-public class GerprapApiApplication {
+public class GerprapApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GerprapApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerprapApiApplication.class, args);
 	}
+
+// }
+
+// @SpringBootApplication
+// public class GerprapApiApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(GerprapApiApplication.class, args);
+// 	}
+	
 
 	@Bean //adding swagger to api documentation -> usage: .../swagger-ui.html <--> create application.description on application.properties file*
 	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
